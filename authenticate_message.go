2f5091060d17589079ff1f5489907d16ff753858
Mamaev [FancaR] Alexandr <fancarster@gmail.com>
2023-12-15 16:17:07 +0300

    modifications for dmd shity ad

--- authenticate_message.go
+++ authenticate_message.go
@@ -80,8 +80,8 @@ func (m authenicateMessage) MarshalBinary() ([]byte, error) {
 	return b.Bytes(), nil
 }
 
-//ProcessChallenge crafts an AUTHENTICATE message in response to the CHALLENGE message
-//that was received from the server
+// ProcessChallenge crafts an AUTHENTICATE message in response to the CHALLENGE message
+// that was received from the server
 func ProcessChallenge(challengeMessageData []byte, user, password string, domainNeeded bool) ([]byte, error) {
 	if user == "" && password == "" {
 		return nil, errors.New("Anonymous authentication not supported")
@@ -98,7 +98,7 @@ func ProcessChallenge(challengeMessageData []byte, user, password string, domain
 	if cm.NegotiateFlags.Has(negotiateFlagNTLMSSPNEGOTIATEKEYEXCH) {
 		return nil, errors.New("Key exchange requested but not supported (NTLMSSP_NEGOTIATE_KEY_EXCH)")
 	}
-	
+
 	if !domainNeeded {
 		cm.TargetName = ""
 	}
@@ -121,57 +121,6 @@ func ProcessChallenge(challengeMessageData []byte, user, password string, domain
 	rand.Reader.Read(clientChallenge)
 
 	ntlmV2Hash := getNtlmV2Hash(password, user, cm.TargetName)
-
-	am.NtChallengeResponse = computeNtlmV2Response(ntlmV2Hash,
-		cm.ServerChallenge[:], clientChallenge, timestamp, cm.TargetInfoRaw)
-
-	if cm.TargetInfoRaw == nil {
-		am.LmChallengeResponse = computeLmV2Response(ntlmV2Hash,
-			cm.ServerChallenge[:], clientChallenge)
-	}
-	return am.MarshalBinary()
-}
-
-func ProcessChallengeWithDomain(challengeMessageData []byte, domain, user, password string, domainNeeded bool) ([]byte, error) {
-	if user == "" && password == "" {
-		return nil, errors.New("Anonymous authentication not supported")
-	}
-
-	var cm challengeMessage
-	if err := cm.UnmarshalBinary(challengeMessageData); err != nil {
-		return nil, err
-	}
-
-	if cm.NegotiateFlags.Has(negotiateFlagNTLMSSPNEGOTIATELMKEY) {
-		return nil, errors.New("Only NTLM v2 is supported, but server requested v1 (NTLMSSP_NEGOTIATE_LM_KEY)")
-	}
-	if cm.NegotiateFlags.Has(negotiateFlagNTLMSSPNEGOTIATEKEYEXCH) {
-		return nil, errors.New("Key exchange requested but not supported (NTLMSSP_NEGOTIATE_KEY_EXCH)")
-	}
-
-	targetName := domain
-	if !domainNeeded {
-		targetName = ""
-	}
-
-	am := authenicateMessage{
-		UserName:       user,
-		TargetName:     targetName,
-		NegotiateFlags: cm.NegotiateFlags,
-	}
-
-	timestamp := cm.TargetInfo[avIDMsvAvTimestamp]
-	if timestamp == nil { // no time sent, take current time
-		ft := uint64(time.Now().UnixNano()) / 100
-		ft += 116444736000000000 // add time between unix & windows offset
-		timestamp = make([]byte, 8)
-		binary.LittleEndian.PutUint64(timestamp, ft)
-	}
-
-	clientChallenge := make([]byte, 8)
-	rand.Reader.Read(clientChallenge)
-
-	ntlmV2Hash := getNtlmV2Hash(password, user, targetName)
 
 	am.NtChallengeResponse = computeNtlmV2Response(ntlmV2Hash,
 		cm.ServerChallenge[:], clientChallenge, timestamp, cm.TargetInfoRaw)
@@ -226,6 +175,58 @@ func ProcessChallengeWithHash(challengeMessageData []byte, user, hash string) ([
 		return nil, err
 	}
 	ntlmV2Hash := hmacMd5(hashBytes, toUnicode(strings.ToUpper(user)+cm.TargetName))
+
+	am.NtChallengeResponse = computeNtlmV2Response(ntlmV2Hash,
+		cm.ServerChallenge[:], clientChallenge, timestamp, cm.TargetInfoRaw)
+
+	if cm.TargetInfoRaw == nil {
+		am.LmChallengeResponse = computeLmV2Response(ntlmV2Hash,
+			cm.ServerChallenge[:], clientChallenge)
+	}
+	return am.MarshalBinary()
+}
+
+// Mostly resembles ProcessChallenge, except usage domain argument instead of challenge message's targetName
+func ProcessChallengeWithDomain(challengeMessageData []byte, domain, user, password string, domainNeeded bool) ([]byte, error) {
+	if user == "" && password == "" {
+		return nil, errors.New("Anonymous authentication not supported")
+	}
+
+	var cm challengeMessage
+	if err := cm.UnmarshalBinary(challengeMessageData); err != nil {
+		return nil, err
+	}
+
+	if cm.NegotiateFlags.Has(negotiateFlagNTLMSSPNEGOTIATELMKEY) {
+		return nil, errors.New("Only NTLM v2 is supported, but server requested v1 (NTLMSSP_NEGOTIATE_LM_KEY)")
+	}
+	if cm.NegotiateFlags.Has(negotiateFlagNTLMSSPNEGOTIATEKEYEXCH) {
+		return nil, errors.New("Key exchange requested but not supported (NTLMSSP_NEGOTIATE_KEY_EXCH)")
+	}
+
+	targetName := domain
+	if !domainNeeded {
+		targetName = ""
+	}
+
+	am := authenicateMessage{
+		UserName:       user,
+		TargetName:     targetName,
+		NegotiateFlags: cm.NegotiateFlags,
+	}
+
+	timestamp := cm.TargetInfo[avIDMsvAvTimestamp]
+	if timestamp == nil { // no time sent, take current time
+		ft := uint64(time.Now().UnixNano()) / 100
+		ft += 116444736000000000 // add time between unix & windows offset
+		timestamp = make([]byte, 8)
+		binary.LittleEndian.PutUint64(timestamp, ft)
+	}
+
+	clientChallenge := make([]byte, 8)
+	rand.Reader.Read(clientChallenge)
+
+	ntlmV2Hash := getNtlmV2Hash(password, user, targetName)
 
 	am.NtChallengeResponse = computeNtlmV2Response(ntlmV2Hash,
 		cm.ServerChallenge[:], clientChallenge, timestamp, cm.TargetInfoRaw)
